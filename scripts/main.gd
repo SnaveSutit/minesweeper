618a4b37c1ab0f342fba208e71f5f9d7637de563
SnaveSutit <snavesutit@gmail.com>
2024-04-29 18:46:07 -0400

    yay mines

---
+++ scripts/main.gd
@@ -0,0 +1,95 @@
+extends Node
+
+const TILE = preload ("res://scenes/tile.tscn")
+@export var boardSize = Vector2(16, 16)
+@export var tileSize = Vector2(32, 32)
+@export var tileMargin = Vector2(4, 4)
+
+@onready var winLabel: Label = $WinLabel
+@onready var loseLabel: Label = $LoseLabel
+
+var grid = []
+var bombCount = 0
+
+func _ready() -> void:
+	winLabel.hide()
+	loseLabel.hide()
+
+	var windowSizeY = get_viewport().size.y
+	boardSize = round(Vector2(windowSizeY, windowSizeY) / tileSize)
+	print(boardSize)
+	build_grid()
+
+func get_neighbors(tile) -> Array:
+	var neighbors = []
+	var i = grid.find(tile)
+	for x in range( - 1, 2):
+		for y in range( - 1, 2):
+			if x == 0 and y == 0:
+				continue
+			var pos = Vector2(i % int(boardSize.x), i / int(boardSize.x)) + Vector2(x, y)
+			if pos.x < 0 or pos.x >= boardSize.x or pos.y < 0 or pos.y >= boardSize.y:
+				continue
+			neighbors.append(grid[int(pos.x + pos.y * boardSize.x)])
+	return neighbors
+
+func build_grid() -> void:
+	if grid.size() > 0:
+		for tile in grid:
+			tile.queue_free()
+	grid.clear()
+
+	var windowSize = Vector2(get_viewport().size)
+	var start = (windowSize - boardSize * (tileSize + tileMargin)) / 2 \
+			+ (tileSize + tileMargin) / 2
+
+	for x in range(boardSize.x):
+		for y in range(boardSize.y):
+			var tile = TILE.instantiate()
+			add_child(tile)
+			grid.append(tile)
+			tile.position = start + Vector2(x, y) * (tileSize + tileMargin)
+			tile.scale = tileSize / tile.spriteSize
+			if randf() > 0.8:
+				tile.isBomb = true
+				bombCount += 1
+				tile.bombs = -1
+			tile.on_reveal.connect(on_tile_revealed)
+			tile.on_flag.connect(on_tile_flagged)
+
+	for i in range(grid.size()):
+		var tile = grid[i]
+		var bombs = 0
+		if not tile.isBomb:
+			var neighbors = get_neighbors(tile)
+			for neighbor in neighbors:
+				if neighbor.isBomb:
+					bombs += 1
+			tile.bombs = bombs
+
+func on_tile_revealed(tile) -> void:
+	if tile.isBomb:
+		loseLabel.show()
+		for otherTile in grid:
+			if not otherTile.isRevealed:
+				otherTile.reveal(false)
+	else:
+		reveal_neighbors(tile)
+
+func on_tile_flagged(tile) -> void:
+	var correctly_flagged = 0
+	for otherTile in grid:
+		if otherTile.isBomb and otherTile.isFlag:
+			correctly_flagged += 1
+	if correctly_flagged == bombCount:
+		winLabel.show()
+
+func reveal_neighbors(tile) -> void:
+	var neighbors = get_neighbors(tile)
+	for neighbor in neighbors:
+		if not neighbor.animating \
+				and not neighbor.isRevealed \
+				and not neighbor.isFlag \
+				and not neighbor.isBomb \
+				and (neighbor.bombs <= 1 or tile.bombs == 0):
+			neighbor.reveal()
---
+++ scripts/tile.gd
@@ -0,0 +1,106 @@
+extends Node2D
+
+signal on_reveal(tile)
+signal on_flag(tile)
+
+const TILE_TEX = preload ("res://sprites/tile.tres")
+const BOMB_TEX = preload ("res://sprites/tile_bomb.tres")
+const FLAG_TEX = preload ("res://sprites/tile_flagged.tres")
+const REVEALED_TEX = preload ("res://sprites/tile_revealed.tres")
+
+@export var animationDuration = 0.25
+
+@export var isBomb = false:
+	set(value):
+		isBomb = value
+		_on_texture_update()
+@export var isFlag = false:
+	set(value):
+		isFlag = value
+		_on_texture_update()
+@export var isRevealed = false:
+	set(value):
+		isRevealed = value
+		_on_texture_update()
+
+@onready var sprite: Sprite2D = $TileSprite
+@onready var spriteSize: Vector2 = sprite.texture.get_size()
+@onready var bombCountLabel: Label = $BombCountLabel
+
+var bombs = 0:
+	set(value):
+		bombs = value
+		if value > 0:
+			bombCountLabel.text = str(value)
+		else:
+			bombCountLabel.text = ""
+var animating = false
+
+func _ready() -> void:
+	sprite.texture = TILE_TEX
+	bombCountLabel.hide()
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if animating or isRevealed or not sprite.get_rect().has_point(to_local(event.position)):
+			return
+
+		if event.button_index == MOUSE_BUTTON_LEFT and not isFlag:
+			reveal()
+
+		elif event.button_index == MOUSE_BUTTON_RIGHT and not isRevealed:
+			toggleFlag()
+
+func reveal(sendSignal=true) -> void:
+	if isFlag:
+		return
+	var originalScale = scale
+	var targetScale = scale * 1.2
+	var tween = create_tween()
+	tween.tween_property(self, 'scale', targetScale, animationDuration / 2) \
+		.set_ease(Tween.EASE_IN_OUT) \
+		.set_trans(Tween.TRANS_EXPO)
+	tween.tween_callback(func():
+		isRevealed=true
+		bombCountLabel.show()
+		if isBomb:
+			bombCountLabel.text="X"
+		if sendSignal: on_reveal.emit(self)
+	)
+	tween.tween_property(self, 'scale', originalScale, animationDuration / 2) \
+		.set_ease(Tween.EASE_IN_OUT) \
+		.set_trans(Tween.TRANS_EXPO)
+	tween.play()
+	animating = true
+	tween.finished.connect(func():
+		animating=false
+	)
+
+func toggleFlag() -> void:
+	var originalScale = scale
+	var targetScale = scale * 1.2
+	var tween = create_tween()
+	tween.tween_property(self, 'scale', targetScale, animationDuration / 2) \
+		.set_ease(Tween.EASE_OUT) \
+		.set_trans(Tween.TRANS_BACK)
+	tween.tween_callback(func():
+		isFlag=!isFlag
+		if isFlag: on_flag.emit(self)
+	)
+	tween.tween_property(self, 'scale', originalScale, animationDuration / 2) \
+		.set_ease(Tween.EASE_OUT) \
+		.set_trans(Tween.TRANS_BACK)
+	tween.play()
+	animating = true
+	tween.finished.connect(func(): animating=false)
+
+func _on_texture_update():
+	if isRevealed:
+		if isBomb:
+			sprite.texture = BOMB_TEX
+		else:
+			sprite.texture = REVEALED_TEX
+	elif isFlag:
+		sprite.texture = FLAG_TEX
+	else:
+		sprite.texture = TILE_TEX
